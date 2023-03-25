fa6fb62d286638271d9d573ad18eb5351aa4bd0e
Carlos Salamanca <casoxsalamanca0190@gmail.com>
2023-03-25 11:38:26 -0500

    Fin seccion 4 Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,36 @@
+//Modulos
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
 import { AppRoutingModule } from './app-routing.module';
-import { PagesComponent } from './pages/pages/pages.component';
+
+import { AuthModule } from './auth/auth.module';
+
+//Componentes
+import { AppComponent } from './app.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { PagesModule } from './pages/pages.module';
+
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
     NopagefoundComponent,
-    DashboardComponent,
-    BreadcrumbsComponent,
-    SidebarComponent,
-    HeaderComponent,
-    ProgressComponent,
-    Grafica1Component,
-    PagesComponent,
+
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule,
+    AuthModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,42 @@
+
+
+//Modulos
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
+
+//Componentes
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { PagesComponent } from './pages/pages.component';
+
+
+
+
+
+
+
+@NgModule({
+  declarations: [
+  DashboardComponent,
+  ProgressComponent,
+  Grafica1Component,
+  PagesComponent,
+
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule
+
+  ],
+  exports:[
+  DashboardComponent,
+  ProgressComponent,
+  Grafica1Component,
+  PagesComponent,
+  ]
+})
+export class PagesModule { }
