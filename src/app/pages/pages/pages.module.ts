0a9017c49e22e154ddeae0faa24b5163cb66bb2e
Carlos Salamanca <casoxsalamanca0190@gmail.com>
2023-03-28 01:58:39 -0500

    Fin seccion 4 - Router

---
+++ src/app/pages/pages/pages.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//Componentes
+import { DashboardComponent } from '../dashboard/dashboard.component';
+import { ProgressComponent } from '../progress/progress.component';
+import { Grafica1Component } from '../grafica1/grafica1.component';
+import { PagesComponent } from './pages.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent
+  ],
+  exports: [], 
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule
+  ]
+})
+export class PagesModule { }
