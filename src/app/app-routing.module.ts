9d9ba56d37137110c8dbad259013f60b942b4b1f
Carlos Salamanca <carlossalamanca0190@gmail.com>
2023-03-30 12:52:23 -0500

    Fin seccion 6

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+//Modulos
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
+
+//Componentes
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+const routes: Routes = [
+
+    {path:'', redirectTo:'/dashboard',pathMatch:'full'},
+    
+
+    //path: '/dashboard' PagesRouting
+    //path: 'auth' AuthRouting
+    { path: '**', component: NopagefoundComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes),
+        PagesRoutingModule,
+        AuthRoutingModule
+    ]
+    ,
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+//Modulos 
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module';
+
+//Componentes
+import { AppComponent } from './app.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    // LoginComponent,
+    // RegisterComponent,
+    NopagefoundComponent,
+   
+  
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PagesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
